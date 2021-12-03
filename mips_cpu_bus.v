bc159063b37506ee636cbb4cff78427f442b69f1
Yrrap <angus@Angus-Laptop.localdomain>
2021-12-03 13:23:12 +0000

    lw attempt

--- mips_cpu_bus.v
+++ mips_cpu_bus.v
@@ -37,6 +37,17 @@ module mips_cpu_bus(
 
         /*-------------------------------------------------*/
 
+        /*----LW combinational things----------------------*/
+        
+        assign read = ((state == stateDecode) && (instructionOpcode == opcodeLW));
+        assign address = ((state == stateDecode) && (instructionOpcode == opcodeLW)) ? (instructionSource1 + instructionImmediateI) << 4 : 32'h00000000;
+        // ^ setting address to read from to be what's dictated by the instruction
+        assign registerWriteEnable = ((state == stateExecute) && (instructionOpcode == opcodeLW)) ? 1 : 0;
+        assign registerWriteAddress = ((state == stateExecute) && (instructionOpcode == opcodeLW)) ? instructionSource2 : 4'b0000;
+        assign registerDataIn = ((state == stateExecute) && (instructionOpcode == opcodeLW)) ? readdata : 32'h00000000;
+        
+        /*-------------------------------------------------*/
+
         /*-----ALU things----*/
 
         logic [3:0] AluControl;
