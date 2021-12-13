d203baf6be416d4a98ae084bf02a901efc86fd43
Yrrap <angus@Angus-Laptop.localdomain>
2021-12-13 11:20:57 +0000

    start of tb

---
+++ test/mips_cpu_bus_tb.v
@@ -0,0 +1,41 @@
+module mips_cpu_bus_tb();
+	logic clk;
+    logic reset;
+    logic active;
+    logic [31:0] register_v0;
+    logic [31:0] address;
+    logic write;
+    logic read;
+    logic waitrequest;
+    logic [31:0] writedata;
+    logic [31:0] byteenable;
+    logic [31:0] readdata;
+
+    mips_cpu_bus cpu(
+        .clk            (clk),
+        .reset          (reset),
+        .active         (active),
+        .register_v0    (register_v0),
+        .address        (address),
+        .write          (write),
+        .read           (read),
+        .waitrequest    (waitrequest),
+        .writedata      (writedata),
+        .byteenable     (byteenable),
+        .readdata       (readdata)
+    );
+
+    initial begin // writing the clock
+        $dumpfile("mips_cpu_bus_tb.vcd");
+        $dumpvars(0, mips_cpu_bus_tb);
+        clk = 0;
+        #5;
+        repeat (100000) begin
+            #10 clk = !clk;
+        end
+        $fatal(2, "Simulation did not finish within 100000 cycles.");
+    end
+
+    initial begin
+        
+    end
