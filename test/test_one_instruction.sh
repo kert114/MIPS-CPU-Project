b879ff2904d7d7179e37a0d09d6b0ff04c444cfa
Yrrap <angus@Angus-Laptop.localdomain>
2021-12-13 14:51:13 +0000

    Getting there

--- test/test_one_instruction.sh
+++ test/test_one_instruction.sh
@@ -1,57 +1,64 @@
+#!/bin/bash
+
 set -eou pipefail
+cd ..
 
 #no variant as we are only testing bus
-INSTR = "$1"
+Folder="$1"
 
-CASES = "test/testcases/${INSTR}/*.asm" #multiple testcases files per instr
+INSTR="$2"
 
-for i in ${CASES}; do
-	TESTNAME=$(basename ${i} .asm) #get name of individual test
-	>&2 echo "Testing MIPS_BUS using test-case ${TESTNAME}" #based on MU0 .sh
+CASES="test/testcase/${INSTR}*.asm.txt" #multiple testcase files per instr
 
-	>&2 echo " 1 - Assembling input file"
-	expected=$(< test/testcases/${INSTR}}/${TESTNAME}.ref) #expected output
+>&2 echo "Testing MIPS_BUS using test-case ${INSTR}" #based on MU0 .sh
 
-	#CHECK OUT MU0 ASSEMBLER FOR MORE stuff to put here**********
+>&2 echo "2 - Compiling test-bench"
 
-	>& echo " 2 - Compiling test-bench"
+#CHECK OUT MU0 - should just be a iverilog command****
 
-	#CHECK OUT MU0 - should just be a iverilog command****
+>&2 echo "3 - Running test-bench"
 
-	>& echo " 3 - Running test-bench"
-	set +e #don't know what this does but copied from MU0
-	#run test and store output to .stdout file
-	test/testcases/${INSTR}/${TESTNAME} > test/testcases/${INSTR}/${TESTNAME}.stdout
-	#not sure what to do with the .vcd file
-	RESULT=$?
-	set -e
+iverilog -Wall -g 2012 \
+	-s mips_cpu_bus_tb test/mips_cpu_bus_tb_mem.v test/mips_cpu_bus_tb.v ${Folder}/mips_cpu_.v \
+	-P mips_cpu_bus_tb.RAM_FILE=\"test/bin/${INSTR}.hex.txt\" \
+	-o test/sim/${INSTR}
 
-	if [[ "${RESULT}" -ne 0 ]] ; then
-   		echo "  ${VARIANT}, ${TESTCASE}, Fail"
-   		exit
-	fi
+set +e #disables automatic script failure if command fails
+#run test and store output to .stdout file
 
-	>& echo "4 - Outputting final v0 value"
-	PATTERN="MIPS v0:" #depends on tb.v outputs
-	NOTHING=""
+test/testcase/${INSTR} > test/output/${INSTR}.stdout
+#not sure what to do with the .vcd file
+RESULT=$?
 
-	set +e
-    grep "${PATTERN}" test/testcases/${INSTR}/${TESTNAME}.stdout > test/testcases/${INSTR}/${TESTNAME}.out-lines
-    sed -e "s/${PATTERN}/${NOTHING}/g" test/testcases/${INSTR}/${TESTNAME}.out-lines > test/testcases/${INSTR}/${TESTNAME}.out
-    set -e #i think this would need to be redone depending on what out tb.v is
+set -e
 
-    >& echo "5 - Comparing output"
-    set +e
-    diff -w test/testcases/${INSTR}/${TESTNAME}.out test/testacses/${INSTR}/${TESTNAME}.ref
-    RESULT=$?
-    set -e
+if [[ "${RESULT}" -ne 0 ]] ; then
+	echo "${INSTR}, Fail"
+	exit
+fi
+>&2 echo "4 - Outputting final v0 value"
+PATTERN="MIPS v0:" #depends on tb.v outputs
+NOTHING=""
 
-    if [[ "${RESULT}" -ne 0 && "$var" != "$fail" ]] ; then
-    	echo "${TESTNAME} ${INSTR} Fail - Mismatch reference" 
-    else
-    	echo "${TESTNAME} ${INSTR} Pass" 
-    fi
-done
+set +e
+
+grep "${PATTERN}" test/testcase/${INSTR}/${TESTNAME}.stdout > test/testcase/${INSTR}/${TESTNAME}.out-lines
+sed -e "s/${PATTERN}/${NOTHING}/g" test/testcase/${INSTR}/${TESTNAME}.out-lines > test/testcase/${INSTR}/${TESTNAME}.out
+set -e #i think this would need to be redone depending on what out tb.v is
+>&2 echo "5 - Comparing output"
+
+set +e
+
+diff -w test/testcase/${INSTR}/${TESTNAME}.out test/testacses/${INSTR}/${TESTNAME}.ref
+RESULT=$?
+
+set -e
+
+if [[ "${RESULT}" -ne 0 && "$var" != "$fail" ]] ; then
+	echo "${TESTNAME} ${INSTR} Fail - Mismatch reference" 
+else
+	echo "${TESTNAME} ${INSTR} Pass" 
+fi
 
 #I have NO CLUE what I'm doing but this is a start I guess...
 
