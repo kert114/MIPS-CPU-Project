1f6cf31598a0362b694dc18bf6c3bd1a98d2623c
Yrrap <angus@Angus-Laptop.localdomain>
2021-12-15 20:42:01 +0000

    a bunch of tests that all work

--- test/mips_cpu_bus_tb_mem.v
+++ test/mips_cpu_bus_tb_mem.v
@@ -51,10 +51,10 @@ module mips_cpu_bus_tb_mem(
 			$display("temp addr is %d, %d, %d, %d", tempaddress, tempaddress+1, tempaddress+2, tempaddress+3);
 			$display("memory is %h, %h, %h, %h",  memory[tempaddress], memory[tempaddress+1], memory[tempaddress+2], memory[tempaddress+3]);
 
-			readdata[31:24] <= memory[tempaddress];
-			readdata[23:16] <= memory[tempaddress+1];
-			readdata[15:8] <= memory[tempaddress+2];
-			readdata[7:0] <= memory[tempaddress+3];
+			readdata[7:0] <= memory[tempaddress];
+			readdata[15:8] <= memory[tempaddress+1];
+			readdata[23:16] <= memory[tempaddress+2];
+			readdata[31:24] <= memory[tempaddress+3];
 		end
 		else if(read == 1 && waitrequest == 0 && dontread == 1) begin
 			dontread = 0;
@@ -72,20 +72,20 @@ module mips_cpu_bus_tb_mem(
 			$display("byteenable is %b", byteenable);
 
 			if (byteenable[0] == 1) begin
-				memory[tempaddress] <= writedata[31:24];
-				$write("%h",writedata[31:24]);
+				memory[tempaddress] <= writedata[7:0];
+				$write("%h",writedata[7:0]);
 			end
 			if (byteenable[1] == 1) begin
-				memory[tempaddress+1] <= writedata[23:16];
-				$write("%h",writedata[23:16]);
-			end
-			if (byteenable[2] == 1) begin
-				memory[tempaddress+2] <= writedata[15:8];
+				memory[tempaddress+1] <= writedata[15:8];
 				$write("%h",writedata[15:8]);
 			end
+			if (byteenable[2] == 1) begin
+				memory[tempaddress+2] <= writedata[23:16];
+				$write("%h",writedata[23:16]);
+			end
 			if (byteenable[3] == 1) begin
-				memory[tempaddress+3] <= writedata[7:0];
-				$write("%h",writedata[7:0]);
+				memory[tempaddress+3] <= writedata[31:24];
+				$write("%h",writedata[31:24]);
 			end
 
 		end
@@ -97,10 +97,10 @@ module mips_cpu_bus_tb_mem(
 			$display("temp addr is %d, %d, %d, %d", tempaddress, tempaddress+1, tempaddress+2, tempaddress+3);
 			$display("memory is %h, %h, %h, %h",  memory[tempaddress], memory[tempaddress+1], memory[tempaddress+2], memory[tempaddress+3]);
 
-			readdata[31:24] <= memory[tempaddress];
-			readdata[23:16] <= memory[tempaddress+1];
-			readdata[15:8] <= memory[tempaddress+2];
-			readdata[7:0] <= memory[tempaddress+3];
+			readdata[7:0] <= memory[tempaddress];
+			readdata[15:8] <= memory[tempaddress+1];
+			readdata[23:16] <= memory[tempaddress+2];
+			readdata[31:24] <= memory[tempaddress+3];
 			dontread = 1;
 		end
 	end
